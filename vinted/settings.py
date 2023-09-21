bd23162dd89b9774df1487c76bbb3683e1227591
Mehdi Karkour <143955961+mkarkour@users.noreply.github.com>
2023-09-21 22:13:25 +0200

    Update settings.py

--- vinted/settings.py
+++ vinted/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4t*g@$&8w3z7bilu0___iq-q7&j07zheuy@#j6i#4we(7g4kq2'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
